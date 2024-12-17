eadd30a6df039f9b0cb1458f75cd6450268d23d4
Afgod-95 <afgod98@gmail.com>
2024-12-17 10:08:16 +0000

    Added lazy loading and code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
-import Home from "./pages/Home";
 import Logo from "../src/assets/PNG/NAA_AMANUA.png";
 import { motion } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
-import About from "./pages/About";
-import News from "./pages/News";
+import { Typography } from "@mui/material";
+
+const Home = React.lazy(() => import("./pages/Home"));
+const About = React.lazy(() => import("./pages/About"));
+const News = React.lazy(() => import("./pages/News"));
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -20,51 +22,49 @@ function App() {
   }, []);
 
   const location = useLocation();
-  const validPaths = ['/', '/about']
+  const validPaths = ['/', '/about'];
 
-  //show nav bar
+  // Show NavBar only on certain paths
   const showNavBar = validPaths.includes(location.pathname);
 
-  
-
-  const PageLoading = () => {
-    return (
-      <motion.div
+  // Loading Component
+  const PageLoading = () => (
+    <motion.div
+      style={{
+        position: "fixed",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+      className="loading"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.img
+        src={Logo}
         style={{
-          position: "fixed",
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center"
+          width: "300px",
+          height: "auto",
         }}
-        className="loading"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <motion.img
-          src={Logo}
-          style={{
-            width: "300px",
-            height: "auto",
-          }}
-          alt="logo"
-          initial={{ y: 0 }}
-          animate={{
-            y: [0, -20, 0],
-          }}
-          transition={{
-            duration: 1,
-            repeat: Infinity,
-            repeatType: "loop",
-          }}
-        />
-      </motion.div>
-    );
-  };
+        alt="logo"
+        initial={{ y: 0 }}
+        animate={{
+          y: [0, -20, 0],
+        }}
+        transition={{
+          duration: 1,
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+      />
+      <Typography variant = 'body1' style={{color: "#000"}}>Please wait while we load the page...</Typography>
+    </motion.div>
+  );
 
   return (
     <>
@@ -72,12 +72,14 @@ function App() {
         <PageLoading />
       ) : (
         <>
-         {showNavBar && <NavBar />}
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/news" element={<News />} />
-          </Routes>
+          {showNavBar && <NavBar />}
+          <Suspense fallback={<PageLoading />}>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </Suspense>
         </>
       )}
     </>
